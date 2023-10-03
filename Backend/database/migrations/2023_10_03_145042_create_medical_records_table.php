f7082aa9effdecb2252565489d5e6363bfee031e
AshkanKazemii <ashkan.kazemi.pln@gmail.com>
2023-10-03 19:21:51 +0330

    Database design and implementation of models and migrations

---
+++ Backend/database/migrations/2023_10_03_145042_create_medical_records_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medical_records', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users' , 'id')->cascadeOnDelete();
+            $table->foreignId('desease_id')->constrained('deseases' , 'id')->cascadeOnDelete();
+            $table->foreignId('payment_id')->constrained('payments', 'id')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medical_records');
+    }
+};
