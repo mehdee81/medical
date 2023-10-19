72df80593403d3d7b09c0814a21136bae97c05c5
Mori 30R <ghahreman00@gmail.com>
2023-10-19 12:26:27 +0330

    Change Data Pattern For National Code

--- Frontend/src/constants/dataPatterns.js
+++ Frontend/src/constants/dataPatterns.js
@@ -3,6 +3,7 @@ import * as yup from "yup";
 const requiredMessage = "این فیلد حتما باید پر بشه";
 const minMessage = (min) => `تعداد کاراکتر حداقل باید ${min} تا باشد `;
 const matchMessage = "رمز وارد شده با رمز عبور یکسان نیست";
+const numberMessage = "مقدار وارد شده باید عدد باشد";
 
 const first_name = yup.string().required(requiredMessage).min(3, minMessage(3));
 const last_name = yup.string().required(requiredMessage).min(3, minMessage(3));
@@ -11,7 +12,8 @@ const age = yup.string().required(requiredMessage).min(1, minMessage(1));
 const province_id = yup.string().required(requiredMessage);
 const city_id = yup.string().required(requiredMessage);
 const national_code = yup
-    .string()
+    .number()
+    .typeError(numberMessage)
     .required(requiredMessage)
     .min(3, minMessage(3));
 const password2 = yup
